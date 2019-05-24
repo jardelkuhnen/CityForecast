5291fed2d62ccd48551951d71d168e98eb9b0fc6
Jardel <jardel.kuhnen@mv.com.br>
2019-05-24 16:56:38 -0300

    Adicionado correções nos components

--- CityForecastWeb/src/app/city/city.component.ts
+++ CityForecastWeb/src/app/city/city.component.ts
@@ -2,7 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { City } from '../model/City';
 import { CityService } from '../services/CityService';
+import { Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
 
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-city',
@@ -13,8 +22,12 @@ export class CityComponent implements OnInit {
   constructor(){}
 
   ngOnInit() {
-
   }
 
+  cidadeForm = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
 }
