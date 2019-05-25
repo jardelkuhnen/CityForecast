f8ea37854838dbe57826887c33f845e5c735381e
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2019-05-25 15:55:19 -0300

    Correçoes do cors

--- CityForecastServ/src/main/java/br/com/hbsis/configuration/WebConfiguration.java
+++ CityForecastServ/src/main/java/br/com/hbsis/configuration/WebConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfiguration {
@@ -18,12 +19,10 @@ public class WebConfiguration {
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
-
-		return new WebMvcConfigurer() {
-
+		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 			}
 		};
 	}
