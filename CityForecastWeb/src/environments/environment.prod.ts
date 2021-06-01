97f763b9e01945fb1bda024a3e48bb8ac2cecceb
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2021-06-01 20:56:55 -0300

    Added dockerfile

--- CityForecastWeb/src/environments/environment.prod.ts
+++ CityForecastWeb/src/environments/environment.prod.ts
@@ -1,9 +1,15 @@
 export const environment = {
   production: true,
 
-  SERVICE_CITY_GET_ALL: "http://localhost:8080/api/city",
+  // SERVICE_CITY_GET_ALL: "http://localhost:8080/api/city",
 
-	SERVICE_CITY_SAVE: "http://localhost:8080/api/city/save/",
+	// SERVICE_CITY_SAVE: "http://localhost:8080/api/city/save/",
 
-	SERVICE_CITY_FORECAST: "http://localhost:8080/api/city/forecast/"
+	// SERVICE_CITY_FORECAST: "http://localhost:8080/api/city/forecast/"
+
+    SERVICE_CITY_GET_ALL: "http://localhost:8080/api/city",
+
+    SERVICE_CITY_SAVE: "http://localhost:8080/api/city/save/",
+
+    SERVICE_CITY_FORECAST: "http://localhost:8080/api/city/forecast/"
 };
